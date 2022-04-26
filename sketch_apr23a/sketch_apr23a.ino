625bfef31ad62577129647fd2d2a9cfcc34d52c0
Andrew Combs <andrewcombs13@gmail.com>
2022-04-25 23:43:45 -0400

    Add A2DP stream
    It compiles and runs but doesn't sound good. I think maybe the copier needs a bigger buffer.

--- sketch_apr23a/sketch_apr23a.ino
+++ sketch_apr23a/sketch_apr23a.ino
@@ -1,35 +1,33 @@
-// Kit-bashed from examples
-// basic-a2dp-audiokit
-// 
+/* Kit-bashed from examples
+ * basic-a2dp-audiokit
+ * streams-a2dp-serial
+ */ 
 
 #include "AudioTools.h"
 #include "AudioLibs/AudioKit.h"
 #include "AudioLibs/AudioA2DP.h"
 
-
-BluetoothA2DPSink a2dp_sink;
-AudioKitStream kit;
-
-// Write data to AudioKit in callback
-void read_data_stream(const uint8_t *data, uint32_t length) {
-    kit.write(data, length);
-}
+A2DPStream a2dp_in = A2DPStream::instance(); // A2DP input - A2DPStream is a singleton!
+AudioKitStream kit_out;
+StreamCopy copier(kit_out, a2dp_in); // copy in to out
 
 void setup() {
   Serial.begin(115200);
   AudioLogger::instance().begin(Serial, AudioLogger::Info);
 
   // setup output
-  auto cfg = kit.defaultConfig(TX_MODE);
+  auto cfg = kit_out.defaultConfig(TX_MODE);
   cfg.sd_active = false;
   cfg.bits_per_sample = 16;
-  kit.begin(cfg);
+  kit_out.begin(cfg);
 
   // register callback
-  a2dp_sink.set_stream_reader(read_data_stream, false);
-  a2dp_sink.start("AudioKit");
+  auto cfg2 = a2dp_in.defaultConfig(RX_MODE);
+  cfg2.name = "Rain Speaker";
+  a2dp_in.begin(cfg2);  
 }
 
 void loop() {
-  kit.processActions();
+  kit_out.processActions();
+  copier.copy();
 }
