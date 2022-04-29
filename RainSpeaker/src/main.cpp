c2372f7ad602f4698ac9a916bec3658aac730435
Andrew Combs <andrewcombs13@gmail.com>
2022-04-28 21:54:42 -0400

    Try to make stream work
    Tried some other stuff to make stream work. A buffer size of 100 worked pretty well at first, but slowly got worse over time. Buffer size of 10 doesn't work at all. Looking at the AudioCopy implementation more it looks like there's a ton of delays in it to handle errors and stuff but I think it's just not gonna work. Going to have to switch back to the callback version. I think I can make that work though.

--- RainSpeaker/src/main.cpp
+++ RainSpeaker/src/main.cpp
@@ -3,13 +3,15 @@
  * streams-a2dp-serial
  */ 
 
+#define COPY_DELAY_ON_NODATA 0
+
 #include "AudioTools.h"
 #include "AudioLibs/AudioKit.h"
 #include "AudioLibs/AudioA2DP.h"
 
 A2DPStream a2dp_in = A2DPStream::instance(); // A2DP input - A2DPStream is a singleton!
 AudioKitStream kit_out;
-StreamCopy copier(kit_out, a2dp_in, 10000); // copy in to out
+StreamCopy copier(kit_out, a2dp_in, 10); // copy in to out
 
 void setup() {
   Serial.begin(115200);
