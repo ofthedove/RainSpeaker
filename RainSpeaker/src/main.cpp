9ad9fa7fe0ebd5e24d83055e410dcfd74d83934e
Andrew Combs <andrewcombs13@gmail.com>
2022-05-06 22:32:21 -0400

    Trying to mix streams
    It's not going well

--- RainSpeaker/src/main.cpp
+++ RainSpeaker/src/main.cpp
@@ -1,35 +1,45 @@
 /* Kit-bashed from examples
  * basic-a2dp-audiokit
- * streams-a2dp-serial
- */ 
-
-#define COPY_DELAY_ON_NODATA 0
+ * ~streams-a2dp-serial~ That didn't work...
+ */
 
 #include "AudioTools.h"
 #include "AudioLibs/AudioKit.h"
 #include "AudioLibs/AudioA2DP.h"
+#include "knghtsng.h"
 
-A2DPStream a2dp_in = A2DPStream::instance(); // A2DP input - A2DPStream is a singleton!
-AudioKitStream kit_out;
-StreamCopy copier(kit_out, a2dp_in, 10); // copy in to out
+BluetoothA2DPSink a2dp_sink;
+AudioKitStream kit;
+
+MemoryStream wav(knghtsng_wav, knghtsng_wav_len);
+MemoryStream *noiseStream = new MemoryStream();
+AdapterAudioStreamToAudioPrint noisePrint(*noiseStream);
+EncodedAudioStream enc(&noisePrint, new WAVDecoder());
+StreamCopy copier(enc, wav);    // copy in to out
+
+void read_data_stream(const uint8_t *data, uint32_t length) {
+  for(uint32_t i = length; i > 0; i--)
+  {
+    uint8_t sample = (uint8_t)noiseStream->read();
+    kit.write(&sample, 1);
+  }
+}
 
 void setup() {
   Serial.begin(115200);
-  // AudioLogger::instance().begin(Serial, AudioLogger::Info);
+  AudioLogger::instance().begin(Serial, AudioLogger::Info);
 
   // setup output
-  auto cfg = kit_out.defaultConfig(TX_MODE);
+  auto cfg = kit.defaultConfig(TX_MODE);
   cfg.sd_active = false;
   cfg.bits_per_sample = 16;
-  kit_out.begin(cfg);
+  kit.begin(cfg);
 
   // register callback
-  auto cfg2 = a2dp_in.defaultConfig(RX_MODE);
-  cfg2.name = "Rain Speaker";
-  a2dp_in.begin(cfg2);  
+  a2dp_sink.set_stream_reader(read_data_stream, false);
+  a2dp_sink.start("Rain Speaker");
 }
 
 void loop() {
-  kit_out.processActions();
-  copier.copy();
+  kit.processActions();
 }
