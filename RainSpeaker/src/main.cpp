9c5a7533771693a4585b4df08a3ac58ed17e175a
Andrew Combs <andrewcombs13@gmail.com>
2022-08-15 23:06:48 -0400

    Speaker use tasks
    Use tasks to avoid all the weird millis stuff, each thing has it's own timing and priority, nice

--- RainSpeaker/src/main.cpp
+++ RainSpeaker/src/main.cpp
@@ -7,6 +7,11 @@
 
 #include "MyAudioPlayer.h"
 
+enum
+{
+  taskStackSize = 10000,
+};
+
 MySdAudioSource *mySource = new MySdAudioSource();
 AudioKitStream kit;
 WAVDecoder decoder;
@@ -20,6 +25,76 @@ uint8_t prevVol;
 int actualVolume = 70;
 
 int greenLedPin = selfAudioKit->pinGreenLed();
+
+TaskHandle_t BleTaskHandle;
+TaskHandle_t VolumeTaskHandle;
+TaskHandle_t AudioTaskHandle;
+
+void BluetoothTask(void *parameter)
+{
+  (void)parameter;
+
+  while (true)
+  {
+    delay(500);
+
+    bt.Run();
+  }
+}
+
+void VolumeTask(void *parameter)
+{
+  (void)parameter;
+
+  while (true)
+  {
+    delay(50);
+
+    int delta = volume - prevVol;
+    if (delta != 0)
+    {
+      prevVol = volume;
+
+      if (delta > 128)
+      {
+        delta = delta - 256;
+      }
+
+      if (delta < -128)
+      {
+        delta = delta + 256;
+      }
+
+      actualVolume += delta;
+
+      if (actualVolume < 0)
+      {
+        actualVolume = 0;
+      }
+
+      if (actualVolume > 100)
+      {
+        actualVolume = 100;
+      }
+
+      float vol = (float)actualVolume / 100.0;
+      player.setVolume(vol);
+    }
+  }
+}
+
+void AudioTask(void *parameter)
+{
+  (void)parameter;
+
+  while (true)
+  {
+    delay(1);
+
+    player.copy();
+    kit.processActions();
+  }
+}
 
 void setup()
 {
@@ -41,47 +116,13 @@ void setup()
   // setup player
   player.setVolume(0.7);
   player.begin();
+
+  xTaskCreate(BluetoothTask, "BLE Task", taskStackSize, NULL, 3, &BleTaskHandle);
+  xTaskCreate(VolumeTask, "Volume Task", taskStackSize, NULL, 2, &VolumeTaskHandle);
+  xTaskCreate(AudioTask, "Audio Task", taskStackSize, NULL, 5, &AudioTaskHandle);
 }
 
 void loop()
 {
-  static unsigned long nextMilli = millis() + 250;
-
-  // Need to handle overflow
-  if (millis() > nextMilli)
-  {
-    nextMilli += 250;
-    bt.Run();
-
-    int delta = volume - prevVol;
-    prevVol = volume;
-
-    if (delta > 128)
-    {
-      delta = delta - 256;
-    }
-
-    if (delta < -128)
-    {
-      delta = delta + 256;
-    }
-
-    actualVolume += delta;
-
-    if (actualVolume < 0)
-    {
-      actualVolume = 0;
-    }
-
-    if (actualVolume > 100)
-    {
-      actualVolume = 100;
-    }
-
-    float vol = (float)actualVolume / 100.0;
-    player.setVolume(vol);
-  }
-
-  player.copy();
-  kit.processActions();
+  delay(1000);
 }
