673bed7f0245d54ca9bbe67ac36b12f6db951851
Andrew Combs <andrewcombs13@gmail.com>
2025-09-24 23:40:33 -0400

    Improve Ramping
    Since I'm here..... Ramp time is no longer the same for any volume change regardless of magnitude. Now ramp time is the time it would take to ramp from min to max volume. Anything changes of smaller magnitude have their duration reduced proportional to the reduction in magnitude.
    
    Like, if ramp time is 2000, ramping from 0 to 100 will take 2 seconds, but ramping from 50 to 100 will take 1 second, and 50 to 75 will take half a second.
    
    Also use some map functions becasue that's all this is.

--- RainSpeaker_Hass/RainSpeaker3/src/AudioWithRamp.cpp
+++ RainSpeaker_Hass/RainSpeaker3/src/AudioWithRamp.cpp
@@ -1,6 +1,13 @@
 #include "AudioWithRamp.h"
 #include <Arduino.h>
 
+enum
+{
+    MinVolume = 0,
+    MaxVolume = 100,
+    MinDuration = 0,
+};
+
 AudioWithRamp::AudioWithRamp(Print& logPrint, int initialVolume, int rampTimeMs) : Audio(logPrint) {
     this->rampTimeMs = rampTimeMs;
     Audio::setVolume(0); // Ramp from 0 to initialVolume
@@ -16,27 +23,30 @@ int AudioWithRamp::getRampTime() {
 }
 
 void AudioWithRamp::setVolume(int volume) {
-    if(volume < 0)
+    if(volume < MinVolume)
     {
-        volume = 0;
+        volume = MinVolume;
     }
-    else if(volume > 100)
+    else if(volume > MaxVolume)
     {
-        volume = 100;
+        volume = MaxVolume;
     }
 
     targetVolume = volume;
     startVolume = getVolume();
     startTimeMs = millis();
+
+    int deltaVolume = abs(targetVolume - startVolume);
+    currentRampDurationMs = map(deltaVolume, MinVolume, MaxVolume, MinDuration, rampTimeMs);
 }
 
 void AudioWithRamp::loop() {
-    int elapsed = millis() - startTimeMs;
-    if (elapsed >= rampTimeMs) {
+    int elapsed = millis() - startTimeMs;\
+
+    if (elapsed >= currentRampDurationMs) {
         Audio::setVolume(targetVolume);
     } else {
-        float progress = (float)elapsed / rampTimeMs;
-        int currentVolume = startVolume + (int)((targetVolume - startVolume) * progress);
+        int currentVolume = map(elapsed, 0, currentRampDurationMs, startVolume, targetVolume);
         Audio::setVolume(currentVolume);
     }
 
--- RainSpeaker_Hass/RainSpeaker3/src/AudioWithRamp.h
+++ RainSpeaker_Hass/RainSpeaker3/src/AudioWithRamp.h
@@ -6,9 +6,14 @@
 class AudioWithRamp : public Audio {
 public:
     AudioWithRamp(Print& logPrint, int initialVolume = 50, int rampTimeMs = 2000);
+
+    // Ramp time is the time it takes to ramp from 0 to full volume in ms
     void setRampTime(int timeMs);
     int getRampTime();
+
+    // Set the target volume that will be ramped to
     void setVolume(int volume);
+
     void loop();
 
 private:
@@ -17,6 +22,7 @@ private:
 
     int startTimeMs;
     int startVolume;
+    int currentRampDurationMs;
 };
 
 #endif
